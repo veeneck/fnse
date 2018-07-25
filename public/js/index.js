fc60c30863bda79225f5b0aac2725b9b2daec2f9
Ryan Campbell <veeneck@gmail.com>
2018-07-25 10:18:23 -0400

    progress on mobile sliding

--- public/js/index.js
+++ public/js/index.js
@@ -80,6 +80,45 @@ $(document).ready(function() {
 		el.parent().next().toggleClass("hide");
 	} 
 
+	  $('.sample-projects').slick({
+		  infinite: false,
+		  speed: 300,
+		  slidesToShow: 3,
+		  arrows: false,
+		   responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow: 3,
+		        slidesToScroll: 1,
+		        dots: false
+		      }
+		    },
+		    {
+		      breakpoint: 600,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1,
+		        dots: true,
+		        dotsClass: "dotstyle",
+		        	  centerMode: true,
+  			variableWidth: true,
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1,
+		        dots: true
+		      }
+		    }
+		    // You can unslick at a given breakpoint now by adding:
+		    // settings: "unslick"
+		    // instead of a settings object
+		  ]
+	  });
+
 });
 
 ;(function($, win) {
