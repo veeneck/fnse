b7a82d2b8dc200e206a33757fe4e660abd0b49d6
Ryan Campbell <veeneck@gmail.com>
2018-07-28 14:35:29 -0400

    js focus

--- themes/charity/static/js/index.js
+++ themes/charity/static/js/index.js
@@ -44,12 +44,12 @@ $(document).ready(function() {
 	          // Callback after animation
 	          // Must change focus!
 	          var $target = $(target);
-	          $target.focus();
+	          ///$target.focus();
 	          if ($target.is(":focus")) { // Checking if the target was focused
 	            return false;
 	          } else {
 	            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-	            $target.focus(); // Set focus again
+	            ///$target.focus(); // Set focus again
 	          };
 	        });
 	      }
