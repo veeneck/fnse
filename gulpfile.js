ba678dfdc28f9f579baf01585721d5ef89099559
Ryan Campbell <veeneck@gmail.com>
2018-08-05 10:39:31 -0400

    Gulp

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var responsive = require('gulp-responsive');
+
+gulp.task('hello', function() {
+  console.log('Hello Zell');
+});
+
+gulp.task("img", () =>
+  gulp.src("./public/images/**.*")
+    .pipe(responsive({
+      "*": [{
+        width: 480,
+        rename: {suffix: "-sm"},
+        skipOnEnlargement: true,
+        withoutEnlargement: true
+      }, {
+        width: 480 * 2,
+        rename: {suffix: "-sm@2x"},
+        skipOnEnlargement: true,
+        withoutEnlargement: true
+      }, {
+        width: 675,
+        skipOnEnlargement: true,
+        withoutEnlargement: true
+      }, {
+        width: 675 * 2,
+        rename: {suffix: "@2x"},
+        skipOnEnlargement: true,
+        withoutEnlargement: true
+      }],
+    }, {
+      silent: true,      // Don't spam the console
+      errorOnEnlargement: false
+    }))
+    .pipe(gulp.dest("./dist/img")
+));
